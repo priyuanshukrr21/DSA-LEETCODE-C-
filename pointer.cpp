6d0729c9344998d51ffa4276c38d8c5d00e95b6a
Priyanshu Kumar <pk2382097@gmail.com>
2026-09-09 22:52:23 +0530

    update

---
+++ pointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+int main() {
+    int a = 10;
+    int* ptr = &a;
+
+    cout << ptr << endl;
+    cout <<&price << endl;
+    return 0;
+}
+
